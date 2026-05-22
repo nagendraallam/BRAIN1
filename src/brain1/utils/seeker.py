55df233846707fd4ff167a66db2ade4b3ab133ea
Nagendra Allam <nagendraallam@icloud.com>
2026-05-22 14:13:22 +0530

    Using rg via subprocess for better seek performance

--- src/brain1/utils/seeker.py
+++ src/brain1/utils/seeker.py
@@ -1,29 +1,16 @@
 from ripgrep_rs import search_structured
 import zstandard as zstd
 import os
+import subprocess
 
-def search_file(file_path: str, query: str) -> list[dict]:
-    """
-    Search a file for a query using ripgrep
-    """
-    return search_structured([query], paths=[file_path], max_total=1000)
 
 # seek in compressed file
 def search_compressed_file(compressed_file_path: str, query: str) -> list[dict]:
     """
     Search a compressed file for a query using ripgrep
     """
-    with open(compressed_file_path, "rb") as f:
-        data = f.read()
-    decompressor = zstd.ZstdDecompressor()
-    decompressed_data = decompressor.decompress(data)
+    result = subprocess.run(["rg", "-z", query, compressed_file_path], capture_output=True, text=True)
+    if result.returncode == 0:
+        print("Found in file " + compressed_file_path + ": " + result.stdout)
 
-    with open("decompressed_data.txt", "wb") as f:
-        f.write(decompressed_data)
 
-        
-    matches = search_structured([query], paths=["decompressed_data.txt"], max_total=1000)
-
-    print(f"Found {len(matches)} matches in {compressed_file_path}")
-    os.remove("decompressed_data.txt")  
-    return matches
